cb74eb7f0ad5a541e5829d270a6765e9df01d3bc
KeremVona <keremvona@gmail.com>
2025-04-29 15:17:27 +0300

    upload

--- frontline/src/components/Header.jsx
+++ frontline/src/components/Header.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Header({ name }) {
   return (
-    <h1 className='text-4xl'>Welcome, {name}</h1>
+    <h1 className='text-4xl p-4'>Welcome, {name}</h1>
   )
 }
--- frontline/src/pages/Dashboard.jsx
+++ frontline/src/pages/Dashboard.jsx
@@ -1,9 +1,11 @@
-import { parse } from 'dotenv';
-import React from 'react'
-import { useState } from 'react';
-import { useEffect } from 'react';
-import Navbar from '../components/Navbar';
-import Header from "../components/Header"
+import { parse } from "dotenv";
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import Navbar from "../components/Navbar";
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
+import "./Dashboard.css";
 
 export default function Dashboard({ setAuth }) {
   const [name, setName] = useState("");
@@ -12,13 +14,12 @@ export default function Dashboard({ setAuth }) {
     try {
       const respone = await fetch("http://localhost:5000/dashboard", {
         method: "GET",
-        headers: { token: localStorage.token }
+        headers: { token: localStorage.token },
       });
 
       const parseRes = await respone.json();
       setName(parseRes.username);
-    }
-    catch (err) {
+    } catch (err) {
       console.error(err.message);
     }
   }
@@ -27,7 +28,7 @@ export default function Dashboard({ setAuth }) {
     getName();
   }, []);
 
-  const logout = async e => {
+  const logout = async (e) => {
     e.preventDefault();
     try {
       localStorage.removeItem("token");
@@ -40,8 +41,48 @@ export default function Dashboard({ setAuth }) {
   return (
     <>
       <Navbar />
-      <Header name={name}/>
-      {/*<button className='bg-amber-100' onClick={e => logout(e)}>Logout</button>*/}
+      <div className="grid grid-cols-2 gap-20">
+        <div id="column-1" className="ml-10 mt-5">
+          <Header name={name} />
+          {/*<button className='bg-amber-100' onClick={e => logout(e)}>Logout</button>*/}
+
+          <ul className="grid grid-cols-2 mt-20 gap-5">
+            <li className="">
+              <Link to="" className="">
+                <p className="text-2xl bg-amber-200 p-2.5">Host Game</p>
+              </Link>
+            </li>
+            <li>
+              <Link to="" className="">
+                <p className="text-2xl bg-amber-200 p-2.5">Game List</p>
+              </Link>
+            </li>
+            <li>
+              <p className="text-2xl bg-amber-200 p-2.5">Friends</p>
+            </li>
+            <li>
+              <p className="text-2xl bg-amber-200 p-2.5">Clan</p>
+            </li>
+          </ul>
+        </div>
+        <div id="column-2" className="mr-10 mt-5">
+          <h2 className="text-4xl p-4 mb-20" id="dashboard">
+            Dashboard
+          </h2>
+          <div className="grid grid-cols-2 gap-2">
+            <div className="grid grid-rows-5 gap-5" id="stats">
+              <p className="text-2xl">Number of games played</p>
+              <p className="text-2xl">Wins</p>
+              <p className="text-2xl">Losses</p>
+              <p className="text-2xl">Draws</p>
+              <p className="text-2xl">Most played</p>
+            </div>
+            <div className="" id="news">
+              <h2 className="text-3xl">News</h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
